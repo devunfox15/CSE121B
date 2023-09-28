5524d5c8522f3913ffe62d10ee7512768672ef11
Devun Durst <devunfoxdurst@yahoo.com>
2023-09-28 14:17:35 -0600

    recommit

---
+++ W02-task/scripts/w03-task.js
@@ -0,0 +1,69 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(number1, number2) {
+    return number1 + number2;
+}
+function addNumbers(){
+let addNumber1 = Number(document.querySelector('#add1').value);
+let addNumber2 = Number(document.querySelector('#add2').value);
+document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+
+function subtract(number1, number2) {
+    return number1 + number2;
+}
+function subtractNumbers(){
+let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+document.querySelector('#difference').value = add(subtractNumber1, subtractNumber2);
+}
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+
+/* Arrow Function - Multiply Numbers */
+
+const multiply = (number1, number2) => number1 * number2;
+
+function multiplyNumbers() {
+    let multiplyNumber1 = Number(document.querySelector('#factor1').value);
+    let multiplyNumber2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2);
+}
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+
+/* Open Function Use - Divide Numbers */
+const divide = (number1, number2) => {
+    if (number2 === 0) {
+        return "Cannot divide by zero!";
+    }
+    return number1 / number2;
+};
+
+function divideNumbers() {
+    let divideNumber1 = Number(document.querySelector('#divide1').value);
+    let divideNumber2 = Number(document.querySelector('#divide2').value);
+    document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
+}
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
